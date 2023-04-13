066e3df204bed58d1c031274892e1a99a3a62e0a
Kent Theo Fourie <42357713+GundamDweeb@users.noreply.github.com>
2023-04-13 20:50:45 +0200

    tests

--- tests/leopard-nft-utils.ts
+++ tests/leopard-nft-utils.ts
@@ -1,90 +1,31 @@
-import { newMockEvent } from "matchstick-as"
-import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import { createMockedFunction, newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt, Bytes, } from "@graphprotocol/graph-ts"
 import {
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
   Transfer
 } from "../generated/LeopardNFT/LeopardNFT"
+import {Leopard, User} from "../generated/schema";
 
-export function createApprovalEvent(
-  owner: Address,
-  approved: Address,
-  tokenId: BigInt
-): Approval {
-  let approvalEvent = changetype<Approval>(newMockEvent())
-
-  approvalEvent.parameters = new Array()
-
-  approvalEvent.parameters.push(
-    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
-  )
-  approvalEvent.parameters.push(
-    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
-  )
-  approvalEvent.parameters.push(
-    new ethereum.EventParam(
-      "tokenId",
-      ethereum.Value.fromUnsignedBigInt(tokenId)
-    )
-  )
-
-  return approvalEvent
-}
-
-export function createApprovalForAllEvent(
-  owner: Address,
-  operator: Address,
-  approved: boolean
-): ApprovalForAll {
-  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
-
-  approvalForAllEvent.parameters = new Array()
-
-  approvalForAllEvent.parameters.push(
-    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
-  )
-  approvalForAllEvent.parameters.push(
-    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
-  )
-  approvalForAllEvent.parameters.push(
-    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
-  )
-
-  return approvalForAllEvent
-}
-
-export function createOwnershipTransferredEvent(
-  previousOwner: Address,
-  newOwner: Address
-): OwnershipTransferred {
-  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
-    newMockEvent()
-  )
-
-  ownershipTransferredEvent.parameters = new Array()
-
-  ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam(
-      "previousOwner",
-      ethereum.Value.fromAddress(previousOwner)
-    )
-  )
-  ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
-  )
-
-  return ownershipTransferredEvent
-}
+export const userOneAddress = Address.fromString("0x0000000000000000000000000000000000000001");
+export const userTwoAddress = Address.fromString("0x0000000000000000000000000000000000000002");
+export const userThreeAddress = Address.fromString("0x0000000000000000000000000000000000000003");
+export const collectionAddress = Address.fromString("0x1000000000000000000000000000000000000004");
+export const ADDRESS_ZERO = Address.fromString("0x0000000000000000000000000000000000000000");
 
 export function createTransferEvent(
   from: Address,
   to: Address,
-  tokenId: BigInt
+  tokenId: BigInt,
+  address: Address,
+  hash: Bytes,
+  blockNumber: BigInt
 ): Transfer {
+  //@ts-ignore
   let transferEvent = changetype<Transfer>(newMockEvent())
 
   transferEvent.parameters = new Array()
+  transferEvent.address = address;
+  transferEvent.transaction.hash = hash;
+  transferEvent.block.number = blockNumber;
 
   transferEvent.parameters.push(
     new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
@@ -100,4 +41,32 @@ export function createTransferEvent(
   )
 
   return transferEvent
+}
+
+export function createLeopard(tokenID: BigInt, collectionAddress: Address, ownerID: string, blockNumber: BigInt): void {
+  let leopard = new Leopard(tokenID.toHexString());
+  leopard.collectionAddress = collectionAddress;
+  leopard.owner = ownerID;
+  leopard.previousOwner = ADDRESS_ZERO.toHexString();
+  leopard.lastModifiedBlock = blockNumber;
+  leopard.tokenID = tokenID;
+  leopard.uri = "IPFS";
+  leopard.metadata = "IPFS";
+  leopard.save()
+}
+
+
+export function createMockedURICall(address: Address, tokenId: BigInt, expectedReturn: string) : void {
+  
+  createMockedFunction(address, "tokenURI", "tokenURI(uint256):(string)")
+  .withArgs([ethereum.Value.fromUnsignedBigInt(tokenId)]).returns([ethereum.Value.fromString("IPFS")])
+}
+
+export function getOrCreateUser(address: Address) : User | null {
+  let user = User.load(address.toHexString());
+  if(user == null) {
+      user = new User(address.toHexString());
+      user.save();
+  }
+  return user;
 }
